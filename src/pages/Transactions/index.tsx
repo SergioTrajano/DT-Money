0f60102b18e528e8e0f0186a6d3762382299f484
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-06-21 10:08:01 -0300

    feat: :sparkles: Add Transactios page

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,45 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+
+import * as S from "./styles";
+
+export function Transactions() {
+    return (
+        <div>
+            <Header />
+
+            <Summary />
+
+            <S.TransactionsContainer>
+                <S.TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td>Desenvolvimento do site</td>
+                            <td>
+                                <S.PriceHighlight variant="income">R$ 1.200,00</S.PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td>Desenvolvimento do site</td>
+                            <td>
+                                <S.PriceHighlight variant="income">R$ 1.200,00</S.PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td>Desenvolvimento do site</td>
+                            <td>
+                                <S.PriceHighlight variant="outcome">- R$ 1.200,00</S.PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                    </tbody>
+                </S.TransactionsTable>
+            </S.TransactionsContainer>
+        </div>
+    );
+}
