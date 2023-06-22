258b636ad8f4b004267683f77fd7b188d6c23f0b
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-06-22 11:35:41 -0300

    feat: :sparkles: Add request to API to render transaction

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
+
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 
 import * as S from "./styles";
 
+interface Transaction {
+    id: number;
+    type: "income" | "outcome";
+    description: string;
+    category: string;
+    price: number;
+    createdAt: string;
+}
+
 export function Transactions() {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_API_URL}/transactions`)
+            .then((response) => response.json())
+            .then((data) => setTransactions(data));
+    }, []);
+
     return (
         <div>
             <Header />
@@ -16,30 +34,18 @@ export function Transactions() {
 
                 <S.TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td>Desenvolvimento do site</td>
-                            <td>
-                                <S.PriceHighlight variant="income">R$ 1.200,00</S.PriceHighlight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/04/2022</td>
-                        </tr>
-                        <tr>
-                            <td>Desenvolvimento do site</td>
-                            <td>
-                                <S.PriceHighlight variant="income">R$ 1.200,00</S.PriceHighlight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/04/2022</td>
-                        </tr>
-                        <tr>
-                            <td>Desenvolvimento do site</td>
-                            <td>
-                                <S.PriceHighlight variant="outcome">- R$ 1.200,00</S.PriceHighlight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/04/2022</td>
-                        </tr>
+                        {transactions.map((transaction) => (
+                            <tr key={transaction.id}>
+                                <td>{transaction.description}</td>
+                                <td>
+                                    <S.PriceHighlight variant={transaction.type}>
+                                        {"R$" + (transaction.price / 100).toFixed(2)}
+                                    </S.PriceHighlight>
+                                </td>
+                                <td>{transaction.category}</td>
+                                <td>{transaction.createdAt}</td>
+                            </tr>
+                        ))}
                     </tbody>
                 </S.TransactionsTable>
             </S.TransactionsContainer>
