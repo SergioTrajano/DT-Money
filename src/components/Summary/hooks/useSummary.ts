dfc16c4d7c8579304858a0e5cfdc70871f73c013
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-06-25 02:04:52 -0300

    refactor: :zap: Use useCallback and useMemo hooks to otimizetion of useSummary custom hook

--- src/components/Summary/hooks/useSummary.ts
+++ src/components/Summary/hooks/useSummary.ts
@@ -1,5 +1,6 @@
 import { useContextSelector } from "use-context-selector";
 
+import { useCallback, useMemo } from "react";
 import { TransactionsContext } from "../../../contexts/TransactionsContext";
 
 interface SummaryProps {
@@ -11,23 +12,28 @@ interface SummaryProps {
 export function useSummary() {
     const transactions = useContextSelector(TransactionsContext, (context) => context.transactions);
 
-    const summary = transactions.reduce(
-        (acumulator, transaction) => calculateSummary(acumulator, transaction),
-        { income: 0, outcome: 0, total: 0 }
+    const calculateSummary = useCallback(
+        (acumulator: SummaryProps, transaction: (typeof transactions)[0]) => {
+            if (transaction.type === "income") {
+                acumulator.income += transaction.price;
+                acumulator.total += transaction.price;
+            }
+            if (transaction.type === "outcome") {
+                acumulator.outcome += transaction.price;
+                acumulator.total -= transaction.price;
+            }
+
+            return acumulator;
+        },
+        []
     );
 
-    function calculateSummary(acumulator: SummaryProps, transaction: (typeof transactions)[0]) {
-        if (transaction.type === "income") {
-            acumulator.income += transaction.price;
-            acumulator.total += transaction.price;
-        }
-        if (transaction.type === "outcome") {
-            acumulator.outcome += transaction.price;
-            acumulator.total -= transaction.price;
-        }
-
-        return acumulator;
-    }
+    const summary = useMemo(() => {
+        return transactions.reduce(
+            (acumulator, transaction) => calculateSummary(acumulator, transaction),
+            { income: 0, outcome: 0, total: 0 }
+        );
+    }, [calculateSummary, transactions]);
 
     return summary;
 }
